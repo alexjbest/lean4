cc18230ffde8134ca8f4b23958bb514e6db97284
Alex J. Best <alex.j.best@gmail.com>
2023-05-03 02:03:34 +0100

    feat: handle simplifying locals with themselves

---
+++ tests/lean/simpHAtH.lean
@@ -0,0 +1,19 @@
+
+axiom q : Nat → Prop
+axiom p : Nat → Prop
+axiom q_eq_p : q = p
+
+example (h : ¬ q 0) : ¬ p 0 := by
+  trace_state
+  /-
+     h : ¬ q 0
+     ⊢ ¬ p 0
+  -/
+  simp [h] at h
+  /-
+     h : ¬ q 0
+     ⊢ ¬ p 0
+  -/
+  trace_state
+  rw [← q_eq_p]
+  assumption
